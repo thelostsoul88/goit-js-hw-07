f0d33a79069baa87979e07ec133e8de0eed5c54f
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-19 16:35:02 +0200

    second task complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galeryImg = document.querySelector(".gallery");
+const imageItemsMarkup = createEl(galleryItems);
+
+function createEl(el) {
+  return el
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
+galeryImg.insertAdjacentHTML("beforeend", imageItemsMarkup);
+galeryImg.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
